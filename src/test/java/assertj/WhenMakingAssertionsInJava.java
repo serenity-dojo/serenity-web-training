b8f8054fe3977a96bc8a0e0c27bc88b04c2fb79a
John Ferguson Smart <john.smart@wakaleo.com>
2021-08-04 10:54:29 +0100

    Sample code

---
+++ src/test/java/assertj/WhenMakingAssertionsInJava.java
@@ -0,0 +1,34 @@
+package assertj;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class WhenMakingAssertionsInJava {
+    @Test
+    public void traditionalAssertions() {
+        int age = 40;
+        List<Integer> ages = Arrays.asList(10, 20, 21, 30);
+
+        assertTrue("The list of ages should contain 40", ages.contains(age));
+    }
+
+    @Test
+    public void assertJAssertions() {
+        int age = 12;
+        List<Integer> ages = Arrays.asList(10, 20, 21, 30);
+
+        // assertThat(age).isGreaterThanOrEqualTo(21);
+
+        assertThat(ages)
+                .contains(10)
+                .hasSize(4)
+                .allMatch(a -> a >= 0 && a <= 100)
+        ;
+
+    }
+}
