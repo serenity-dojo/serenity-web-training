5781b72d053e985eca046a35372a13e8d5717cc0
John Ferguson Smart <john.smart@wakaleo.com>
2021-08-04 12:52:15 +0100

    Starting point

---
+++ src/test/java/seleniumeasy/WhenInteractingWithInputForms.java
@@ -0,0 +1,65 @@
+package seleniumeasy;
+
+import org.junit.Test;
+
+/**
+ * This is a series of exercises designed to explore how to use
+ * Serenity BDD to test various kinds of HTML elements
+ */
+public class WhenInteractingWithInputForms {
+
+    /**
+     * Basic form fields:
+     * Enter a message and check that the message is correctly displayed
+     * https://www.seleniumeasy.com/test/basic-first-form-demo.html
+     */
+    @Test
+    public void basicForms() {
+    }
+
+    /**
+     * Checkboxes
+     * Check that a message appears when you click the checkbox
+     * https://www.seleniumeasy.com/test/basic-checkbox-demo.html
+     */
+    @Test
+    public void checkboxes() {
+    }
+
+    /**
+     * Radio buttons
+     * Check that a message appears when you click the radio button
+     * https://www.seleniumeasy.com/test/basic-radiobutton-demo.html
+     */
+    @Test
+    public void radioButtons() {
+    }
+
+    /**
+     * Dropdown lists
+     * https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html
+     */
+    @Test
+    public void selectList() {
+
+    }
+
+    /**
+     * Multi-Select Dropdown lists
+     * https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html
+     */
+    @Test
+    public void multiSelectList() {
+
+    }
+
+    /**
+     * JQuery Select  list
+     * https://www.seleniumeasy.com/test/jquery-dropdown-search-demo.html
+     */
+    @Test
+    public void jquerySelectList() {
+
+    }
+
+}
