2bd4c8b27e5ebbbe64e30ab72e192199b6c228b1
John Ferguson Smart <john.smart@wakaleo.com>
2021-08-07 11:46:48 +0100

    Starting point

--- src/test/java/seleniumeasy/WhenWaitingForElements.java
+++ src/test/java/seleniumeasy/WhenWaitingForElements.java
@@ -5,11 +5,6 @@ import net.thucydides.core.annotations.Managed;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import seleniumeasy.pageobjects.AlertMessagePage;
-import seleniumeasy.pageobjects.DynamicDataPage;
-import seleniumeasy.pageobjects.DynamicallyLoadingPage;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SerenityRunner.class)
 public class WhenWaitingForElements {
@@ -17,43 +12,16 @@ public class WhenWaitingForElements {
     @Managed(driver = "chrome")
     WebDriver driver;
 
-    AlertMessagePage alertMessagePage;
-
     @Test
     public void waitingForAMessageToClose() {
-        alertMessagePage.open();
-
-        alertMessagePage.openSuccessMessage();
-
-        assertThat(alertMessagePage.alertSuccessMessageText()).contains("I'm an autocloseable success message");
-
-        alertMessagePage.waitForMessageToDissapear();
-        alertMessagePage.alertSuccessMessage().shouldNotBeVisible();
     }
-
-    DynamicallyLoadingPage dynamicallyLoadingPage;
 
     @Test
     public void waitingForElementsToAppear() {
-        dynamicallyLoadingPage.open();
-
-        dynamicallyLoadingPage.start();
-
-        assertThat(dynamicallyLoadingPage.getMessage()).isEqualTo("Hello World!");
     }
-
-    DynamicDataPage dynamicDataPage;
 
     @Test
     public void waitingForElementsToDisappear() {
-        dynamicDataPage.open();
-        ;
-
-        dynamicDataPage.getNewUser();
-
-        assertThat(dynamicDataPage.userDescription())
-                .contains("First Name")
-                .contains("Last Name");
     }
 
 }
