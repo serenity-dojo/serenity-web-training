7189f354aacbf313d50cf4486b1a2ea3df8244e5
John Ferguson Smart <john.smart@wakaleo.com>
2021-08-11 18:39:33 +0100

    Sample solution

--- src/test/java/webtests/seleniumeasy/WhenDownloadingFiles.java
+++ src/test/java/webtests/seleniumeasy/WhenDownloadingFiles.java
@@ -10,9 +10,12 @@ import org.openqa.selenium.WebDriver;
 import webtests.seleniumeasy.pageobjects.DownloadPage;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 @RunWith(SerenityRunner.class)
 public class WhenDownloadingFiles {
@@ -23,20 +26,17 @@ public class WhenDownloadingFiles {
     DownloadPage downloadPage;
 
     @Test
-    public void weCanDownloadAFileToOurHardDrive() {
-
+    public void weCanDownloadAFileToOurHardDrive() throws InterruptedException {
         downloadPage.open();
 
-        // Download a file
-        downloadPage.downloadSamplePngFile();
+        downloadPage.downloadSampleFile();
 
-        // Identify the temporary download directory
         File downloadedFile = SessionLocalTempDirectory.forTheCurrentSession().resolve("sample.png").toFile();
 
-        // Wait for the file to download
-        Awaitility.await().atMost(5, TimeUnit.SECONDS).until(downloadedFile::exists);
+        await().atMost(15, SECONDS).until(downloadedFile::exists);
 
-        // Check that the file exists
         assertThat(downloadedFile).exists();
+
+        assertThat(downloadedFile.getName()).isEqualTo("sample.png");
     }
 }
--- src/test/java/webtests/seleniumeasy/pageobjects/DownloadPage.java
+++ src/test/java/webtests/seleniumeasy/pageobjects/DownloadPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 
 @DefaultUrl("http://the-internet.herokuapp.com/download")
 public class DownloadPage extends PageObject {
-    public void downloadSamplePngFile() {
+    public void downloadSampleFile() {
         $(By.linkText("sample.png")).click();
     }
 }
