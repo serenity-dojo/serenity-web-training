a259587aa020f0d2b77bd9a1963964a63b3e8af8
John Ferguson Smart <john.smart@wakaleo.com>
2021-08-24 13:36:26 +0100

    Sample start

---
+++ src/test/java/todomvc/WhenDeletingATask.java
@@ -0,0 +1,14 @@
+package todomvc;
+
+import org.junit.Test;
+
+public class WhenDeletingATask {
+
+    // TODO: Exercise 5
+    @Test
+    public void deletedItemsShouldDissapearFromTheList() {
+        // Add "Feed the cat" and "Walk the dog" to the list
+        // Delete "Feed the cat"
+        // Check that only "Walk the dog" appears
+    }
+}
