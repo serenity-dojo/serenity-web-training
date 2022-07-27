b54ee6d593d57331f72cbc597b7fe90620d0f4c0
John Ferguson Smart <john.smart@wakaleo.com>
2022-07-27 16:44:27 +0100

    Updated sample

--- src/test/java/webtests/todo/TodoMvcPage.java
+++ src/test/java/webtests/todo/TodoMvcPage.java
@@ -5,12 +5,10 @@ import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.webdriver.javascript.ByShadowDom;
 import org.openqa.selenium.By;
 
-@DefaultUrl("https://todomvc.com/examples/emberjs/")
+@DefaultUrl("https://todomvc.com/examples/angularjs/")
 public class TodoMvcPage extends PageObject {
 
     public String placeholderText() {
-        return find(By.id("new-todo"))
-                .find(ByShadowDom.of("div[pseudo=placeholder]"))
-                .getText();
+        return find(".new-todo").getAttribute("placeholder");
     }
 }
