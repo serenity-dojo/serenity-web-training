403b224233381188797f34730223676dbdd33888
John Ferguson Smart <john.smart@wakaleo.com>
2024-11-08 12:57:21 +0000

    Updated Serenity

--- src/test/java/serenityswag/cart/CartPageObject.java
+++ src/test/java/serenityswag/cart/CartPageObject.java
@@ -2,47 +2,38 @@ package serenityswag.cart;
 
 import net.serenitybdd.annotations.DefaultUrl;
 import net.serenitybdd.core.pages.PageObject;
-import net.serenitybdd.core.pages.WebElementFacade;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.By;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @DefaultUrl("https://www.saucedemo.com/cart.html")
 public class CartPageObject extends PageObject {
 
     public static final String CART_ITEM_DELETE_BUTTON = "//div[@class='cart_item_label'][contains(.,'{0}')]//button";
-
-    @FindBy(id = "checkout")
-    WebElementFacade checkoutButton;
-
-    @FindBy(css = ".title")
-    WebElementFacade title;
-
-    @FindBy(className = "cart_item")
-    List<WebElementFacade> cartItemElements;
+    private static By CHECKOUT_BUTTON = By.id("checkout");
+    private static By TITLE = By.cssSelector(".title");
+    private static By CART_ITEMS = By.cssSelector(".cart_item");
 
     public void checkout() {
-        checkoutButton.click();
+        $(CHECKOUT_BUTTON).click();
     }
 
     public String getTitleText() {
-        return title.getText();
+        return $(TITLE).getText();
     }
 
     public List<CartItem> items() {
-        List<CartItem> cartItems = new ArrayList<>();
-        for(WebElementFacade cartItemElement : cartItemElements) {
-            String name = cartItemElement.findBy(".inventory_item_name").getText();
-            String description = cartItemElement.findBy(".inventory_item_desc").getText();
-            Double price = priceFrom(cartItemElement.findBy(".inventory_item_price").getText());
-            cartItems.add(new CartItem(name, description, price));
-        }
-        return cartItems;
+        return findAll(CART_ITEMS).map(
+                item -> new CartItem(
+                        item.findBy(".inventory_item_name").getText(),
+                        item.findBy(".inventory_item_desc").getText(),
+                        priceFrom(item.findBy(".inventory_item_price").getText())
+                )
+        );
     }
 
     private Double priceFrom(String textValue) {
-        return Double.parseDouble(textValue.replace("$",""));
+        return Double.parseDouble(textValue.replace("$", ""));
     }
 
     public void removeItem(String itemToDelete) {
