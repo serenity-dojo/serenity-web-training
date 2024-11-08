d3f425813df39b7e05dcd6e98a931b4098a95a85
John Ferguson Smart <john.smart@wakaleo.com>
2024-11-08 15:57:42 +0000

    Sample solution

--- src/test/java/serenityswag/toolshop/WhenSearchingForAProduct.java
+++ src/test/java/serenityswag/toolshop/WhenSearchingForAProduct.java
@@ -2,6 +2,8 @@ package serenityswag.toolshop;
 
 import net.serenitybdd.core.steps.UIInteractions;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -12,22 +14,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SerenityJUnit5Extension.class)
 public class WhenSearchingForAProduct extends UIInteractions {
 
+    @BeforeEach
+    void openSite() {
+        openUrl("https://practicesoftwaretesting.com/");
+    }
+
     @Test
     void searchByKeyword() {
-        openUrl("https://practicesoftwaretesting.com/");
-
         // Enter "saw" into the search field
         $("#search-query").typeAndEnter("saw");
 
         // Count the number of products
+        List<String> displayedProducts = getDisplayedProducts();
+
+        assertThat(displayedProducts).contains("Wood Saw", "Circular Saw");
+    }
+
+    @NotNull
+    private List<String> getDisplayedProducts() {
         List<String> displayedProducts
                 = findAll(".card-title")
                 .textContents()
                 .stream()
                 .map(titleValue -> titleValue.strip())
                 .toList();
+        return displayedProducts;
+    }
 
-        assertThat(displayedProducts).contains("Wood Saw", "Circular Saw");
+    @Test
+    void filterSearchResults() {
+
+        var hammerCheckbox = $("#filters").findBy("//label[contains(.,'Hammer')]/input");
+
+        hammerCheckbox.click();
+
+        boolean isChecked = Boolean.parseBoolean(hammerCheckbox.getAttribute("checked"));
+        assertThat(isChecked).isTrue();
+
+        List<String> displayedProducts = getDisplayedProducts();
+        assertThat(displayedProducts).allMatch(
+                name -> name.toLowerCase().contains("hammer")
+        );
+
     }
 
 }
