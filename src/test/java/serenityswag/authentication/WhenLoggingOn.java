74269d1e69743fe136c28a2cc0f2ad53bf0c8902
John Ferguson Smart <john.smart@wakaleo.com>
2024-11-08 11:10:01 +0000

    Updated sample code to JUnit 5

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -1,36 +1,32 @@
 package serenityswag.authentication;
 
-import net.serenitybdd.core.Serenity;
-import net.serenitybdd.junit.runners.SerenityRunner;
-import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Steps;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
-import serenityswag.inventory.InventoryPage;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static serenityswag.authentication.User.STANDARD_USER;
 
-@RunWith(SerenityRunner.class)
+@ExtendWith(SerenityJUnit5Extension.class)
 public class WhenLoggingOn {
 
     @Managed
     WebDriver driver;
 
-    @Steps
-    LoginActions login;
-
-    InventoryPage inventoryPage;
-
     @Test
     public void usersCanLogOnViaTheHomePage() {
 
-        login.as(STANDARD_USER);
+        driver.get("https://www.saucedemo.com");
+
+        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
 
         // Should see product catalog
-        Serenity.reportThat("The inventory page should be displayed with the correct title",
-                () -> assertThat(inventoryPage.getHeading()).isEqualToIgnoringCase("Products")
-        );
+
+        assertThat(driver.findElement(By.cssSelector(".title")).getText())
+                .isEqualToIgnoringCase("Products");
     }
 }
